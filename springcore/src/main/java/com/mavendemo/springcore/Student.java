8bb64d69f9a606ca669310033d9d1d69ad5c1794
Aa-yush <ayushg9990@gmail.com>
2022-05-31 18:34:47 +0530

    Setter Collection Injection

--- springcore/src/main/java/com/mavendemo/springcore/Student.java
+++ springcore/src/main/java/com/mavendemo/springcore/Student.java
@@ -1,9 +1,14 @@
 package com.mavendemo.springcore;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Student {
 	private int studentId;
-	private String studentName;
-	private String studentAddress;
+	private List<String> studentPhone;
+	private Set<String> studentName;
+	private Map<String, String> studentCourses;
 
 	public int getStudentId() {
 		return studentId;
@@ -13,27 +18,37 @@ public class Student {
 		this.studentId = studentId;
 	}
 
-	public String getStudentName() {
+	public List<String> getStudentPhone() {
+		return studentPhone;
+	}
+
+	public void setStudentPhone(List<String> studentPhone) {
+		this.studentPhone = studentPhone;
+	}
+
+	public Set<String> getStudentName() {
 		return studentName;
 	}
 
-	public void setStudentName(String studentName) {
+	public void setStudentName(Set<String> studentName) {
 		this.studentName = studentName;
 	}
 
-	public String getStudentAddress() {
-		return studentAddress;
+	public Map<String, String> getStudentCourses() {
+		return studentCourses;
 	}
 
-	public void setStudentAddress(String studentAddress) {
-		this.studentAddress = studentAddress;
+	public void setStudentCourses(Map<String, String> studentCourses) {
+		this.studentCourses = studentCourses;
 	}
 
-	public Student(int studentId, String studentName, String studentAddress) {
+	public Student(int studentId, List<String> studentPhone, Set<String> studentName,
+			Map<String, String> studentCourses) {
 		super();
 		this.studentId = studentId;
+		this.studentPhone = studentPhone;
 		this.studentName = studentName;
-		this.studentAddress = studentAddress;
+		this.studentCourses = studentCourses;
 	}
 
 	public Student() {
@@ -43,8 +58,8 @@ public class Student {
 
 	@Override
 	public String toString() {
-		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
-				+ "]";
+		return "Student [studentId=" + studentId + ", studentPhone=" + studentPhone + ", studentName=" + studentName
+				+ ", studentCourses=" + studentCourses + "]";
 	}
 
 }
